11b261265c80f883260d6143992a1069f231ac41
aleclofabbro <agiansanti@uniquid.com>
2017-12-07 01:12:29 +0100

    ok senza $ ne start, con default virtuale

--- src/index.hot.js
+++ src/index.hot.js
@@ -3,11 +3,12 @@ require('../assets/css/focus/core.scss')
 
 // javascript
 const R = require('ramda')
+const Rx = require('rxjs')
 const React = require('react')
 const ReactDOM = require('react-dom')
 
 const connect = require('./lib/connect')
-const _focus = require('./lib/focus')
+const Focus = require('./lib/focus')
 const _todos_worker = require('./workers/todos')
 const _todos_service = require('./services/todos')
 
@@ -15,35 +16,35 @@ const _App = require('./view/App')
 const _TodoList = require('./view/TodoList')
 
 
-const be = 'http://localhost:8080/'
-const elem = document.getElementById('focus')
 
-const render = (state) => {
-  console.log(state,'---state---',)
-  ReactDOM.render((
-    <App />
-  ), elem)
-}
+Focus(({focus, set, defaultTo}) => {
+  defaultTo({todos:[{title:'a'},{title:'b'}]})
+  const be = 'http://localhost:8080/'
+  const elem = document.getElementById('focus')
 
-
-if (module.hot) {
-  module.hot.accept('./view/App', render)
-}
-
-
-const {focus, start} = _focus(render, {todos:[{title:'a'},{title:'b'}]})
-const todos_worker = _todos_worker(focus('todos'))
-const todos_service = _todos_service({be})
-const App = connect(_App, (ownProps)=>{
-  return {
-    TodoList
+  const render = (state) => {
+    console.log(state,'---state---',)
+    ReactDOM.render((
+      <App />
+    ), elem)
   }
-})
-const TodoList = connect(_TodoList, (ownProps)=>{
-  return {
-    list: todos_worker.view(),
-    rm: todos_worker.rm
-  }
-})
 
-start()
+  if (module.hot) {
+    module.hot.accept('./view/App', render)
+  }
+
+  const todos_worker = _todos_worker(focus('todos'))
+  const todos_service = _todos_service({be})
+  const App = connect(_App, (ownProps)=>{
+    return {
+      TodoList
+    }
+  })
+  const TodoList = connect(_TodoList, (ownProps)=>{
+    return {
+      list: todos_worker.view(),
+      rm: todos_worker.rm
+    }
+  })
+  return render
+})
--- src/lib/focus.js
+++ src/lib/focus.js
@@ -1,29 +1,26 @@
 const R = require('ramda')
-const Rx = require('rxjs')
 
-const root = (next, init_state) => {
+const root = (init) => {
   let state
   const root = R.lens(() => state, _state => {
-    if(state !== _state){
-      state = _state
+    if(next && state !== _state){
       next(_state)
     }
+    state = _state
   })
   const focus = _focus(root)
-  return {
-    ...focus,
-    start: () => focus.set(init_state)
-  }
+  const next = init(focus)
+  next(state)
 }
 
-const is_empty = v => {
-  return v === null ||
-    v === void 0 ||
-    v !== v
-}
 
 const _focus = (lens, {ns}={}) => {
   let default_to
+  let is_empty = v => {
+    return v === null ||
+      v === void 0 ||
+      v !== v
+  }
   const focus = (on, {ns}={}) => {
     ns = ns ? ns : 'string' === typeof on ? on.split('.') : ns
     on = 'string' === typeof on ? R.lensPath(on.split('.')) : on
@@ -32,12 +29,13 @@ const _focus = (lens, {ns}={}) => {
   }
   const set = R.set(lens, R.__, null)
   const over = R.over(lens, R.__, null)
-  const view = () => R.view(lens, null)
+  const view = () => {
+    const val = R.view(lens, null)
+    return is_empty(val) ? default_to : val
+  }
   const defaultTo = (def, _is_empty) => {
-    if((_is_empty || is_empty)(view())) {
-      default_to = def
-      // set(def)
-    }
+    is_empty = _is_empty || is_empty
+    default_to = def
   }
   return {
     defaultTo,
