69cf5e0eb8fc556e8f878cdf0192fece10328758
aleclofabbro <agiansanti@uniquid.com>
2017-12-09 00:51:46 +0100

    no default in conn

--- src/connections/todos/index.js
+++ src/connections/todos/index.js
@@ -3,16 +3,14 @@ const React = require('react')
 const TodoList = require('../../view/TodoList')
 module.exports = (TodoList_F, index) => {
   const {view, set, over} = TodoList_F
-  // console.log('TodoList conn', view())
-  // over(val => val || [{title:'a'},{title:'b'}])
-  if(!view()){
-    set([{title:`${index}a`},{title:`${index}b`}])
-    return
+  const add = () => {
+    const name = prompt('name')
+    set(R.prepend({title:`title:${name}-${index}`,body:`body:${name}-${index}`}, view()))
   }
-
   const rm = (ix) => set(R.remove(ix, 1, view()))
   const getById = (id) => view().find(todo => todo.id === id)
   const ctl = {
+    add,
     getById,
     list: view(),
     rm
--- src/view/TodoList.js
+++ src/view/TodoList.js
@@ -1,13 +1,17 @@
 const React = require('react')
-module.exports = ({list, rm}) => {
+module.exports = ({list, rm, add}) => {
   // console.log('TodoList view')
   return (
-    <ul>
-      {
-        list.map((todo, ix) => (
-          <li key={ix} onClick={()=>rm(ix)}>{todo.title}</li>
-        ))
-      }
-    </ul>
+    <div>
+      <button onClick={add}>addtodo</button>
+
+      <ul>
+        {
+          list.map((todo, ix) => (
+            <li key={ix} onClick={()=>rm(ix)}>{todo.title}</li>
+          ))
+        }
+      </ul>
+    </div>
   )
 }
