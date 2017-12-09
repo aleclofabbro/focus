56cd53e970330fc63a37bee184ae9f940b5d5012
aleclofabbro <agiansanti@uniquid.com>
2017-12-09 11:41:22 +0100

    Merge branch 'focus-rx'

--- src/lib/focus/index.js
+++ src/lib/focus/index.js
@@ -4,7 +4,7 @@ const Focus = (init, initial_subject) => {
   let subject
   let frame
   let _iterations_frame_cnt = 0
-  const subject_lens = R.lens(() => subject, _next_subject => {
+  const subject_lens = R.lens(() => subject, (_next_subject, __) => {
     console.log(`**`, _next_subject, __)
     // _next_subject = plugins.reduce((__next_subject,plugin)=>plugin(__next_subject, subject, __, subject_focus, frame), _next_subject)
     if(subject !== _next_subject){
